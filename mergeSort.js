14abf454ab5985263a7f208f8a9bfef93db962a8
JOE MON <56827756+JOEMN96@users.noreply.github.com>
2021-10-01 23:41:03 +0530

    mergeSort done

--- mergeSort.js
+++ mergeSort.js
@@ -1,7 +1,8 @@
-var mergeTwoLists = function (l1, l2) {
+function merge(l1, l2) {
   let res = [];
   let i = 0;
   let j = 0;
+
   while (i < l1.length && j < l2.length) {
     if (l1[i] < l2[j]) {
       res.push(l1[i]);
@@ -20,8 +21,15 @@ var mergeTwoLists = function (l1, l2) {
     res.push(l2[j]);
     j++;
   }
-
   return res;
-};
+}
 
-console.log(mergeTwoLists([1, 2, 4], [1, 3, 4, 4, 5, 22]));
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let middle = Math.floor(arr.length / 2);
+  let left = arr.slice(0, middle);
+  let right = arr.slice(middle);
+  return merge(left, right);
+}
+
+console.log(mergeSort([1, 2, 4, 20, 1, 3, 4, 4, 5, 22]));
--- problem.ts
+++ problem.ts
@@ -1,7 +1,6 @@
 var removeElement = function(nums:number [], val:number) {
 
    for (let i = 0; i < nums.length; i++) {
-      console.log(nums[i]);
       
       if(nums[i] == val) {
          nums.splice(i,1)
