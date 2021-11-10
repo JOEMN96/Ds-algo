feee05a3fe288042ae016fe33d57cc6396fc9632
JOE MON <56827756+JOEMN96@users.noreply.github.com>
2021-11-10 22:02:24 +0530

    two pointer challenge

--- problem.ts
+++ problem.ts
@@ -1,29 +1,13 @@
-interface Mapp {
-    [item:number]:number
+function reversePrefix(word: string, ch: string): string {
+  let ours = word.substring(0, word.indexOf(ch) + 1);
+  let ar = word.substring(word.indexOf(ch) + 1);
+  let j = ours.length - 1;
+  let str = '';
+  while (j >= 0) {
+    str += ours[j];
+    j--;
+  }
+  return str + ar;
 }
 
-function kWeakestRows(mat: number[][], k: number): number[] {
-
-    let mapped:Mapp = {};
-    // { '0': 2, '1': 4, '2': 1, '3': 2, '4': -1 }
-    let res = []
-
-    mat.forEach((item,index) => {
-        mapped[index] = item.indexOf(0)
-    })
- 
-    for ( const x in mapped)  {
-        console.log(x);   
-            
-    }
-    console.log(res);
-    
-    return []
-};
-
-kWeakestRows([[1,1,0,0,0],
-    [1,1,1,1,0],
-    [1,0,0,0,0],
-    [1,1,0,0,0],
-    [1,1,1,1,1]], 
-   3)
+console.log(reversePrefix('abcdefd', 'd'));
