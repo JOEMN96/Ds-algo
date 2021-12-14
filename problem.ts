8146e6f55a403e183af0acb4849da646d59fb4b8
JOE MON <56827756+JOEMN96@users.noreply.github.com>
2021-12-14 16:24:20 +0530

    not

--- problem.ts
+++ problem.ts
@@ -1,7 +1,22 @@
-function commonChars(words: string[]): string[] {
-  let res = [''];
-  let i = 0;
-  return res;
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
 }
 
-console.log(commonChars(['bella', 'label', 'roller']));
+function deleteNode(root: ListNode | null): void {
+  let next = root.val;
+  while (next) {
+    console.log(next);
+    next = root.val;
+  }
+}
+
+let list3 = new ListNode(2, null);
+
+let list2 = new ListNode(1, list3);
+
+deleteNode(list2);
--- test.js
+++ test.js
@@ -1,18 +1,34 @@
-function whatIsThis() {
-  console.log(this == 'window');
-}
+// function whatIsThis() {
+//   console.log(this == 'window');
+// }
 
-let arrpw = () => {
-  console.log(this);
-};
+// let arrpw = () => {
+//   console.log(this);
+// };
 
-let a = {
-  b: 100,
-  x: function () {
-    console.log(this.b);
-  },
-};
+// let a = {
+//   b: 100,
+//   x: function () {
+//     console.log(this.b);
+//   },
+// };
 
-let xx = { b: 101 };
+// let xx = { b: 101 };
 
-a.x.call(xx);
+// a.x.call(xx);
+// console.log(1);
+// setTimeout(() => console.log(2));
+// console.log(3);
+// new Promise((resolve, reject) => {
+//   console.log(4);
+// })
+//   .then(() => console.log(5))
+//   .then((res, rej) => {
+//     console.log(6);
+//     res(0);
+//   })
+//   .then(console.log(7));
+
+(function (a, b, c) {
+  console.log(a && b | c);
+})('1', [].length, 100);
