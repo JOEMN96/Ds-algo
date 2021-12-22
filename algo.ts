59ca34eab83cc083bf240a417e43b6b6a74a1d03
JOE MON <56827756+JOEMN96@users.noreply.github.com>
2021-12-22 16:36:39 +0530

    push done - SinglyLinkedList

--- algo.ts
+++ algo.ts
@@ -18,4 +18,43 @@ class DoublyLinkedList {
     this.head = null;
     this.length = 0;
   }
+  push(val: number) {
+    let newNode = new Node(val);
+    if (this.head == null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+
+  pop(): Node | undefined {
+    if (this.length < 1) return undefined;
+    let lastNode = this.tail;
+    this.tail.prev.next = null;
+    this.length--;
+    return lastNode;
+  }
+
+  print(): void {
+    let arr: number[] = [];
+    let curr = this.head;
+    while (curr) {
+      arr.push(curr.val);
+      curr = curr.next;
+    }
+    console.log(arr);
+  }
 }
+
+let list = new DoublyLinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
+console.log(list.pop());
+console.log(list.pop());
+
+list.print();
