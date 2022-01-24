da5a9156e9933126917428e6d21719179e99f54c
Joe Mn <aruljoe37@gmail.com>
2022-01-24 22:15:31 +0530

    selectionSort

---
+++ Sorting/selectionSort.ts
@@ -0,0 +1,20 @@
+function selectionSort(arr: number[]) {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    // If statement is for preventing unnecessary swaps
+    if (1 != lowest) {
+      let temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
+  }
+  console.log(arr);
+}
+
+// On2 Time complexity
+selectionSort([0, 2, 9, 4, 7, 8, 6]);
